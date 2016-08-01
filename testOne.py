28392c7f7733b0d9b15f6ce9ffc111fdcb112579
Paul Krush <pkrush@opey.attlocal.net>
2016-08-01 09:49:09 -0500

    working better

--- testOne.py
+++ testOne.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Make sure that caffe is on the python path:
-caffe_root = '../'  # this file is expected to be in {caffe_root}/examples
+#caffe_root = '/home/pkrush/'  # this file is expected to be in {caffe_root}/examples
 import sys
-sys.path.insert(0, caffe_root + 'python')
-sys.path.append('~/caffe/python')
+#sys.path.insert(0, caffe_root + 'python')
+sys.path.append('/home/pkrush/caffe/python')
 
 import caffe
 
 # Set the right path to your model definition file, pretrained model weights,
 # and the image you would like to classify.
-MODEL_FILE = 'mnist/lenet.prototxt'
-PRETRAINED = 'mnist/lenet_iter_10000.caffemodel'
-IMAGE_FILE = 'mnist/9.png'
+model_dir = 'copper60/'
+MODEL_FILE = model_dir + 'deploy.prototxt'
+PRETRAINED = model_dir + 'snapshot.caffemodel'
+IMAGE_FILE = 'temp.png'
 
 net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(28, 28), raw_scale=255)
 score = net.predict([caffe.io.load_image(IMAGE_FILE, color=False)], oversample=False)
