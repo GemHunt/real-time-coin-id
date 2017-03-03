51c010071cd64915594cdd2d55e1585d5bdda540
Paul Krush <pkrush@GemHunt.com>
2017-03-03 14:21:01 -0600

    Scanning 18 LEDs twice

--- scanning.py
+++ scanning.py
@@ -29,9 +29,8 @@ def deskew(src, pixel_shift):
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200)
 cap = cv2.VideoCapture(0)
-cap.set(38, 0)
-cap.set(3, 1920)
-cap.set(4, 1080)
+cap.set(3, 1280)
+cap.set(4, 1024)
 
 count = 0
 coin_count = 0
@@ -44,24 +43,34 @@ while (True):
     start_time = time.time()
     ret, frame = cap.read()
     # deskewed = deskew(frame, 5)
+    print '1 In %s seconds' % (time.time() - start_time,)
     if frame == None:
         # print 'None in %s seconds' % (time.time() - start_time,)
         continue
     # cv2.imwrite('/home/pkrush/cents-hd/' + str(coin_count).zfill(5) + str(count).zfill(2) + '.png', frame)
-    coin_size_adjustment_factor = 1.03
-    frame_width = int(960 * coin_size_adjustment_factor)
-    frame_hieght = int(540 * coin_size_adjustment_factor)
+    print '2 In %s seconds' % (time.time() - start_time,)
+    # frame = frame[460:,40:1040]
+
+    coin_size_adjustment_factor = 1.06
+    frame_width = int(640 * coin_size_adjustment_factor)
+    frame_hieght = int(512 * coin_size_adjustment_factor)
+    print '3 In %s seconds' % (time.time() - start_time,)
     frame = cv2.resize(frame, (frame_width, frame_hieght), interpolation=cv2.INTER_AREA)
+    print '4 In %s seconds' % (time.time() - start_time,)
 
     cv2.imshow('frame', frame)
     # frame = deskew(frame,-9)  #100 rpm
     # frame = deskew(frame, -30)
+    print '5 In %s seconds' % (time.time() - start_time,)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=50, param2=30, minRadius=448, maxRadius=450)
 
     # good for scanning:
     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=222, maxRadius=226)
+    print '6 In %s seconds' % (time.time() - start_time,)
+
     circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=215, maxRadius=224)
+    print '7 In %s seconds' % (time.time() - start_time,)
 
     if circles is None:
         continue
@@ -71,8 +80,6 @@ while (True):
     print 'circles:', len(circles)
 
 
-
-
     for i in circles[0, :]:
         center_x = i[0]
         center_y = i[1]
@@ -80,7 +87,8 @@ while (True):
         # cv2.circle(frame, (center_x, center_y), 2, (0, 0, 255), 1)
         # print circles
         cv2.imshow('detected circles', frame)
-        center_stop = 400
+        center_stop = 250
+        print center_x, "   ", center_y, "     ", crop_radius
         if center_x > center_stop:
             print center_x
             if found_coin == False:
@@ -90,8 +98,11 @@ while (True):
                 cv.WaitKey(2)
                 continue
             print count
-            ser.write(str(count) + "\n")
-            cv.WaitKey(2)
+            led = count / 2
+            if led < 18:
+                print "led", led
+                ser.write(str(led) + "\n")
+                cv.WaitKey(2)
 
             center_list.append([center_x, center_y, crop_radius])
 
@@ -111,6 +122,8 @@ while (True):
             # average_radius = float(total_radius) / sample_size
 
             # print average_center_x, center_x, "   ", average_center_y, center_y, "     ", average_radius, crop_radius
+            print center_x, "   ", center_y, "     ", crop_radius
+
             font = cv2.FONT_HERSHEY_SIMPLEX
             # cv2.circle(frame, (int(average_center_x), int(average_center_y)), int(average_radius), (0, 255, 0), 1)
             cv2.circle(frame, (center_x, center_y), crop_radius, (0, 255, 0), 1)
@@ -122,10 +135,12 @@ while (True):
             #cv2.putText(crop, str(average_radius)[0:5], (10, 90), font, .7, (0, 255, 0), 2)
 
             cv2.imshow('crop', crop)
+            if count > 4 or count < 40:
+                image_id = count - 5
             filename = '/home/pkrush/cents-test/' + str(coin_count) + str(count).zfill(2) + '.png'
             # print filename
             cv2.imwrite(filename, crop)
-            if found_coin == True and count < 18:
+            if found_coin == True and count < 49:
                 if frame_buffer_count != 0:
                     frame_buffer_count -= 1
                 else:
@@ -136,7 +151,7 @@ while (True):
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-        # print 'In %s seconds' % (time.time() - start_time,)
+    print '8 In %s seconds' % (time.time() - start_time,)
 
 cap.release()
 cv2.destroyAllWindows()
