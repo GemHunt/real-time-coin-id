77e5af032568963a03c3c4ed97af840c2f85f3b5
Paul Krush <pkrush@GemHunt.com>
2017-03-22 15:12:21 -0500

    Working on getting top and bottom scanning flow

--- scanning.py
+++ scanning.py
@@ -1,6 +1,7 @@
+import glob
 import numpy as np
+import os
 import serial
-import threading
 import time
 
 import cv2
@@ -62,142 +63,79 @@ def scan(top_camera, bottom_camera, ser):
             cv.WaitKey(1)
     return top_captures, bottom_captures
 
-
 def save(captures, coin_id):
     count = 0
+    center_list = []
     for frame in captures:
-        ratio = .7
+        # ratio = 1
+        # frame_width = int(1920 * ratio)
+        # frame_height = int(1080 * ratio)
+        # frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
+
+
+
+        if coin_id % 2 == 0:
+            ratio = .405
+        else:
+            ratio = .46
+
         frame_width = int(1920 * ratio)
         frame_height = int(1080 * ratio)
         # print '3 In %s seconds' % (time.time() - start_time,)
         frame = cv2.resize(frame, (frame_width, frame_height), interpolation=cv2.INTER_AREA)
-        if count == 54:
-            cv2.imwrite('/home/pkrush/cents-test/' + str(coin_id).zfill(5) + str(count).zfill(2) + '.png', frame)
-        count += 1
-        #
-        # cv2.imshow('frame', frame)
-        #cv.WaitKey(30)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if coin_id % 2 == 0:
+            # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=50, param2=30, minRadius=448, maxRadius=450)
+            circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 2000, param1=45, param2=25, minRadius=222,
+                                       maxRadius=226)
+        else:
+            circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 2000, param1=45, param2=25, minRadius=222,
+                                       maxRadius=226)
+        if circles is None:
+            continue
+        circles = np.uint16(np.around(circles))
 
-    #     # cv2.imwrite('/home/pkrush/cents-hd/' + str(coin_count).zfill(5) + str(count).zfill(2) + '.png', frame)
-    #     # bottom camera:
-    #     # coin_size_adjustment_factor = 1.06
-    #
-    #     # top_camera:
-    #     coin_size_adjustment_factor = .405
-    #
-    #     # frame = deskew(frame,-9)  #100 rpm
-    #     # frame = deskew(frame, -30)
-    #     # print '5 In %s seconds' % (time.time() - start_time,)
-    #     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=50, param2=30, minRadius=448, maxRadius=450)
-    #
-    #     # good for scanning:
-    #     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=222, maxRadius=226)
-    #     # print '6 In %s seconds' % (time.time() - start_time,)
-    #
-    #     # Bottom Camera
-    #     circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=222, maxRadius=224)
-    #
-    #     # Top Camera:
-    #     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=290, maxRadius=295)
-    #
-    #     # print '7 In %s seconds' % (time.time() - start_time,)
-    #
-    #     if circles is None:
-    #         # continue
-    #         pass
-    #
-    #     circles = np.uint16(np.around(circles))
-    #
-    #     print 'circles:', len(circles)
-    #
-    #     for i in circles[0, :]:
-    #         center_x = i[0]
-    #         center_y = i[1]
-    #         crop_radius = i[2]
-    #         # cv2.circle(frame, (center_x, center_y), 2, (0, 0, 255), 1)
-    #         # cv2.circle(frame, (center_x, center_y), crop_radius, (0, 0, 255), 1)
-    #         # print circles
-    #         cv2.imshow('detected circles', frame)
-    #
-    #         # The coin is move from right to left so the center_x is going down.
-    #         center_stop_x = 530
-    #         center_go_x = 350
-    #         print center_x, "   ", center_y, "     ", crop_radius
-    #         if center_go_x < center_x < center_stop_x:
-    #             print center_x
-    #             if found_coin == False:
-    #                 found_coin = True
-    #                 print "Conveyor Stop"
-    #                 # ser.write(str(103) + "\n")
-    #                 cv.WaitKey(2)
-    #                 continue
-    #             print count
-    #             led = count / 2
-    #             if led < 29:
-    #                 print "led", led
-    #                 # ser.write(str(led) + "\n")
-    #                 cv.WaitKey(2)
-    #
-    #             center_list.append([center_x, center_y, crop_radius])
-    #
-    #             sample_size = 20
-    #
-    #             total_center_x = 0
-    #             total_center_y = 0
-    #             total_radius = 0
-    #
-    #             for past_x, past_y, past_radius in center_list[len(center_list) - sample_size:len(center_list)]:
-    #                 total_center_x += past_x
-    #                 total_center_y += past_y
-    #                 total_radius += past_radius
-    #
-    #             average_center_x = float(total_center_x) / sample_size
-    #             average_center_y = float(total_center_y) / sample_size
-    #             average_radius = float(total_radius) / sample_size
-    #
-    #             print average_center_x, center_x, "   ", average_center_y, center_y, "     ", average_radius, crop_radius
-    #             print center_x, "   ", center_y, "     ", crop_radius
-    #
-    #             font = cv2.FONT_HERSHEY_SIMPLEX
-    #             cv2.circle(frame, (int(average_center_x), int(average_center_y)), int(average_radius), (0, 255, 0), 1)
-    #             # cv2.circle(frame, (center_x, center_y), crop_radius, (0, 255, 0), 1)
-    #
-    #             # crop = frame[center_y - 224:center_y + 224, center_x - 224:center_x + 224]
-    #             # cv2.putText(crop, str(crop_radius)[0:5], (10, 90), font, .7, (0, 255, 0), 2)
-    #             if len(center_list) > 20:
-    #                 crop = frame[average_center_y - 224:average_center_y + 224,
-    #                        average_center_x - 224:average_center_x + 224]
-    #                 cv2.putText(crop, str(average_radius)[0:5], (10, 90), font, .7, (0, 255, 0), 2)
-    #
-    #                 cv2.imshow('crop', crop)
-    #                 if count > 4 and count < 62:
-    #                     image_id = count - 5
-    #                     filename = '/home/pkrush/cents-test/' + str(coin_count) + str(image_id).zfill(2) + '.png'
-    #                     cv2.imwrite(filename, frame)
-    #                 if count == 62:
-    #                     # ser.write(str(102) + "\n")
-    #                     cv.WaitKey(2)
-    #                 if count == 80:
-    #                     # ser.write(str(100) + "\n")
-    #                     # cv.WaitKey(100)
-    #                     # ser.write(str(101) + "\n")
-    #                     count = 0
-    #                     found_coin = False
-    #                     coin_count += 1
-    #
-    #                 if found_coin == True:
-    #                     count += 1
-    #
-    #     # red = frame[:, :, 2]
-    #     # green = frame[:, :, 1]
-    #     # blue = frame[:, :, 0]
-    #
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     return
+        for i in circles[0, :]:
+            center_x = i[0]
+            center_y = i[1]
+            crop_radius = i[2]
+            cv2.circle(gray, (center_x, center_y), 2, (0, 0, 255), 1)
+            cv2.circle(gray, (center_x, center_y), crop_radius, (0, 0, 255), 1)
+            # print circles
+            cv2.imshow('detected circles', gray)
+            cv2.waitKey(20)
+            center_list.append([center_x, center_y, crop_radius])
+            cv2.imwrite('/home/pkrush/cents-test/' + str(coin_id).zfill(5) + str(count).zfill(2) + '.png', gray)
+        count += 1
+
+    total_center_x = 0
+    total_center_y = 0
+    total_radius = 0
+
+    for center_x, center_y, crop_radius in center_list:
+        print center_x, center_y, crop_radius
+        total_center_x += center_x
+        total_center_y += center_y
+        total_radius += crop_radius
+
+    average_center_x = float(total_center_x) / len(center_list)
+    average_center_y = float(total_center_y) / len(center_list)
+    average_radius = float(total_radius) / len(center_list)
+
+    print average_center_x, center_x, "   ", average_center_y, center_y, "     ", average_radius, crop_radius
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.circle(gray, (int(average_center_x), int(average_center_y)), int(average_radius), (0, 255, 0), 1)
+    cv2.circle(gray, (int(average_center_x), int(average_center_y)), 2, (255, 0, 0), 1)
+    cv2.imshow("ave", gray)
+    cv2.waitKey(0)
+
+    # crop = frame[center_y - 224:center_y + 224, center_x - 224:center_x + 224]
+    # cv2.putText(crop, str(crop_radius)[0:5], (10, 90), font, .7, (0, 255, 0), 2)
     return
 
-def get_moving_center_x(frame, ratio, deskew_pixels, frame_name):
+
+def get_moving_center_x(frame, ratio, deskew_pixels, frame_name, frame_id):
     frame_width = int(1920 * ratio)
     frame_height = int(1080 * ratio)
     # print '3 In %s seconds' % (time.time() - start_time,)
@@ -217,7 +155,7 @@ def get_moving_center_x(frame, ratio, deskew_pixels, frame_name):
     # circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=222, maxRadius=226)
     # print '6 In %s seconds' % (time.time() - start_time,)
     # Bottom Camera
-    circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=40, param2=20, minRadius=52, maxRadius=58)
+    circles = cv2.HoughCircles(gray, cv.CV_HOUGH_GRADIENT, 1, 300, param1=45, param2=25, minRadius=52, maxRadius=58)
     if circles is None:
         cv2.imshow(frame_name, frame)
         return 0
@@ -231,7 +169,41 @@ def get_moving_center_x(frame, ratio, deskew_pixels, frame_name):
         cv2.circle(frame, (center_x, center_y), crop_radius, (0, 0, 255), 1)
         # print circles
         cv2.imshow(frame_name, frame)
+        cv2.imwrite('/home/pkrush/cents-circle-detect/' + str(frame_id).zfill(6) + frame_name + '.png', frame)
     return center_x * (1 / ratio)
+
+
+def get_cameras():
+    top_camera = None
+    bottom_camera = None
+
+    for camera_id in range(0, 3):
+        cap = cv2.VideoCapture(camera_id)
+        cap.set(3, 1920)
+        cap.set(4, 1080)
+
+        if cap.get(3) == 1920:
+            if top_camera is None:
+                top_camera = cap
+            else:
+                bottom_camera = cap
+
+    top, bottom = read_from_cameras(top_camera, bottom_camera)
+    if bottom[0, 0, 0] == bottom[0, 0, 1] == bottom[0, 0, 2]:
+        temp_camera = top_camera
+        top_camera = bottom_camera
+        bottom_camera = temp_camera
+    return top_camera, bottom_camera
+
+
+top_camera, bottom_camera = get_cameras()
+
+files = glob.glob('/home/pkrush/cents-circle-detect/*')
+for f in files:
+    os.remove(f)
+files = glob.glob('/home/pkrush/cents-test/*')
+for f in files:
+    os.remove(f)
 
 
 ser = serial.Serial(port='/dev/ttyUSB1', baudrate=115200)
@@ -239,40 +211,17 @@ ser.write(str(102) + "\n")
 cv.WaitKey(2)
 ser.write(str(104) + "\n")
 cv.WaitKey(2)
-cameras = []
-top_camera = None
-bottom_camera = None
-
-for camera_id in range(0, 3):
-    cap = cv2.VideoCapture(camera_id)
-    cap.set(3, 1920)
-    cap.set(4, 1080)
-
-    if cap.get(3) == 1920:
-        if top_camera is None:
-            top_camera = cap
-        else:
-            bottom_camera = cap
-
-            # if cap.get(3) == 1920:
-            #     if bottom_camera is None:
-            #         bottom_camera = cap
-            #     else:
-            #         top_camera = cap
 frame_count = 0
 last_scan_frame_count = -100
 coin_count = 0
 center_list = []
 found_coin = False
 
-top_status = 2
-bottom_status = 2
-print top_status, bottom_status
-#first_top_scanned = False
-
+top_status = 1
+bottom_status = 1
 # 0 is ready to scan
 # 1 is move to center
-# 2 is moving away from center
+
 
 while (True):
     status = ''
@@ -280,72 +229,64 @@ while (True):
     cv.WaitKey(2)
     start_time = time.time()
 
+    top, bottom = read_from_cameras(top_camera, bottom_camera)
     # Wait at least 15 frames before looking again:
-    if frame_count - last_scan_frame_count < 15:
+    if frame_count - last_scan_frame_count < 35:
         frame_count += 1
         continue
 
-    top, bottom = read_from_cameras(top_camera, bottom_camera)
-
-    if top_status in (1, 2):
-        center_x = get_moving_center_x(top, .1, 8, 'Top')
+    if top_status == 1:
+        center_x = get_moving_center_x(top, .1, 8, 'Top', frame_count)
         if center_x != 0:
             status += 'top' + ' ' + str(center_x) + '-'
-            if top_status == 2 and center_x > 1600:
-                top_status = 1
+            if top_status == 1 and center_x < 1400:
+                top_status = 0
                 status += str(top_status) + ' ' + str(bottom_status) + '-'
-            else:
-                if top_status == 1 and center_x < 1600:
-                    # if first_top_scanned == True:
-                    top_status = 0
-                    status += str(top_status) + ' ' + str(bottom_status) + '-'
-                    ser.write(str(105) + "\n")
-                    cv.WaitKey(2)
-                    status += 'Top belt off'
-                    # else:
-                    # first_top_scanned = True
-                    #top_status = 2
+                ser.write(str(105) + "\n")
+                cv.WaitKey(1)
+                status += 'Top belt off'
 
-    if bottom_status in (1, 2):
-        center_x = get_moving_center_x(bottom, .11, -8, 'Bottom')
+    if bottom_status == 1:
+        center_x = get_moving_center_x(bottom, .11, -8, 'Bot', frame_count)
         if center_x != 0:
             status += 'bottom' + ' ' + str(center_x) + '-'
-            if bottom_status == 2 and center_x < 500:
-                bottom_status = 1
+            if bottom_status == 1 and center_x > 470:
+                bottom_status = 0
                 status += str(top_status) + ' ' + str(bottom_status) + '-'
-            else:
-                if bottom_status == 1 and center_x > 300:
-                    bottom_status = 0
-                    status += str(top_status) + ' ' + str(bottom_status) + '-'
-                    ser.write(str(103) + "\n")
-                    status += 'Bottom belt off'
+                ser.write(str(103) + "\n")
+                cv.WaitKey(1)
+                status += 'Bottom belt off-'
 
     if top_status == 0 and bottom_status == 0:
         # if first_top_scanned == True:
+        status += 'Scanning ' + str(coin_count) + ' with the LED lights-'
+        last_scan_frame_count = frame_count
+        #cv.WaitKey(60)
         top_captures, bottom_captures = scan(top_camera, bottom_camera, ser)
-
-        t = threading.Thread(target=save, args=(top_captures, coin_count))
-        t.start()
-        t = threading.Thread(target=save, args=(bottom_captures, coin_count + 1))
-        t.start()
-        coin_count += 2
-        status += 'Cycle In %s seconds' % (time.time() - start_time,)
-        start_time = time.time()
-        status += 'Scanning with the LED lights.'
+        # t = threading.Thread(target=save, args=(top_captures, coin_count))
+        # t.start()
+        # t = threading.Thread(target=save, args=(bottom_captures, coin_count + 1))
+        # t.start()
+        ser.write(str(101) + "\n")
         cv.WaitKey(10)
-        ser.write(str(102) + "\n")
-        cv.WaitKey(2)
-        ser.write(str(104) + "\n")
-        cv.WaitKey(2)
-        status += 'Both belts on'
-        top_status = 2
-        bottom_status = 2
+        save(top_captures, coin_count)
+        save(bottom_captures, coin_count + 1)
+        ser.write(str(100) + "\n")
+        cv.WaitKey(30)
+        coin_count += 2
+        #status += 'Cycle In %s seconds' % (time.time() - start_time,)
+        start_time = time.time()
+        status += 'Both belts on-'
+        top_status = 1
+        bottom_status = 1
         status += str(top_status) + ' ' + str(bottom_status) + '-'
-    if top_status == 2 and bottom_status == 2:
+    if top_status == 1 and bottom_status == 1:
+        ser.write(str(100) + "\n")
+        cv.WaitKey(1)
         ser.write(str(102) + "\n")
-        cv.WaitKey(2)
+        cv.WaitKey(1)
         ser.write(str(104) + "\n")
-        cv.WaitKey(2)
+        cv.WaitKey(1)
     if status != '':
         print frame_count, status
     frame_count +=1
@@ -357,5 +298,6 @@ cv.WaitKey(35)
 cv.WaitKey(100)
 #ser.write(str(101) + "\n")
 
-cap.release()
+top_camera.release()
+bottom_camera.release()
 cv2.destroyAllWindows()
